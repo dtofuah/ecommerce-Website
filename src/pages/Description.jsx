b67e0082a43c4602d3e6131b563a51e39e2853eb
Donald Tofuah <dtofuah@gmail.com>
2017-08-13 13:12:07 -0400

    total

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -2,7 +2,6 @@
 import React, { Component } from "react";
 import PRODUCTS from "json/products.json";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 import { Button } from "semantic-ui-react";
 
 class Description extends Component {
@@ -11,38 +10,38 @@ class Description extends Component {
 
 		this.state = {
 
-
 		};
 	}
-		_handleAddCart = (productId) => {
-			this.props.addCart(this.props.product.id);
-		};
 
-		render() {
-			const { product } = this.props;
-			// const product = PRODUCTS[this.state.productId];
-			return (
-			 <div className = "Description">
+	_handleAddCart = (productId) => {
+		this.props.addCart(this.props.product.id);
+	}
+	render() {
+		const { product } = this.props;
+		return (
+			<div className = "Description">
 
 				  	<h1>{product.name}</h1>
 				   	<p>
 								Category: {product.category}
-				 </p>
-					<p>
+							</p>
+							<p>
 								Price: ${product.price}
-					</p>
-					<p> Discription: {product.description}</p>
-					<p> Ratings: {product.rating}</p>
-					<img src = {product.images[1].medium}/>
+							</p>
+							<p> Discription: {product.description}</p>
+							<p> Ratings: {product.rating}</p>
+							<img src = {product.images[1].medium}/>
 						 <img src = {product.images[0].medium}/>
 
-
 						 <div className="product-add-description">
-							 <Button value = {product.id} onClick = {this._handleAddCart}>
-								 ADD TO CART </Button>
-							 </div>
-				</div>
-			);
+						 	<Button value = {product.id} onClick = {this._handleAddCart}>
+						 		ADD TO CART </Button>
+						 	</div>
+						</div>
+
+
+		);
 		}
 }
+
 export default Description;
